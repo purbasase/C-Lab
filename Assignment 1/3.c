7394106ad924d77f24532d966ad0513ecd050a51
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-08-18 21:47:32 +0530

    Create 3.c

---
+++ Assignment 1/3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void update_array(int *arr,int size);
+int main()
+{
+	int s;
+	printf("Enter size of array :");
+	scanf("%d",&s);
+	int a[s];
+	printf("Enter Array Elements :\n");
+	for(int i=0;i<s;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("The array elements in the main function (Before updation) :\n");
+	for(int i=0;i<s;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+	update_array(a,s); //calling the function by array reference
+	printf("The array elements in the main function (After updation) :\n");
+	for(int i=0;i<s;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+	return 0;
+}
+void update_array(int *arr,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		arr[i]+=15; //updating array by adding 15 with each element
+	}
+	printf("The array elements in the called function (After updation) :\n");
+	for(int i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+}
