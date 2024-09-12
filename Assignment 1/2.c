1a5f4c376ad0d3e28fc7f63a0232896e03321178
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-12 21:33:11 +0530

    2.c

--- Assignment 1/2.c
+++ Assignment 1/2.c
@@ -1,40 +1,18 @@
+//Write a C program to find the sum of individual digits of a given positive integer.
+
+
 #include<stdio.h>
-void update_array(int x[],int size)
+void main()
 {
-        //creating a local copy of the array
-        int arr[size];
-        for(int i=0;i<size;i++)
-        {
-                arr[i]=x[i]+24; //updating array in the function by adding twenty five
-        }
-        printf("The Array In the calling funcation (After updation) :\n");
-        for(int i=0;i<size;i++)
-        {
-                printf("%d ",arr[i]);
-        }
-        printf("\n");
+int n,s=0;
+printf("\n**Sum of Digits of a Positive Integer**\n");
+printf("Name: Purbasa Seal, Class: MCA1A, Roll: 40\n");
+printf("Enter a Positive Integer Number :");
+scanf("%d",&n);
+while(n!=0) //Logic to find the sum of digits
+{
+s=s+(n%10);
+n=n/10;
 }
-int main()
-{
-        int s;
-        printf("Enter size of array :");
-        scanf("%d",&s);
-        int a[s];
-        printf("Enter %d elements for array :\n",s);
-        for(int i=0;i<s;i++)
-        {
-                scanf("%d",&a[i]);
-        }
-        printf("The Array In the main function (Before updation) :\n");
-        for(int i=0;i<s;i++)
-        {
-                printf("%d ",a[i]);
-        }
-        printf("\n");
-        update_array(a,s); //call by value
-        printf("The Array In the main function (After updation) :\n");
-        for(int i=0;i<s;i++)
-        {
-                printf("%d ",a[i]);
-        }
+printf("Sum of digits = %d\n",s);
 }
