cf59a0627ea207917568f15c5fa5464d312748ee
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-12 21:34:21 +0530

    3.c

--- Assignment 1/3.c
+++ Assignment 1/3.c
@@ -1,41 +1,23 @@
+//Write a C program to generate the first n terms of the Fibonacci sequence.
+
 #include<stdio.h>
-void update_array(int *arr,int size);
-int main()
+void main()
 {
-	int s;
-	printf("Enter size of array :");
-	scanf("%d",&s);
-	int a[s];
-	printf("Enter Array Elements :\n");
-	for(int i=0;i<s;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	printf("The array elements in the main function (Before updation) :\n");
-	for(int i=0;i<s;i++)
-	{
-		printf("%d ",a[i]);
-	}
-	printf("\n");
-	update_array(a,s); //calling the function by array reference
-	printf("The array elements in the main function (After updation) :\n");
-	for(int i=0;i<s;i++)
-	{
-		printf("%d ",a[i]);
-	}
-	printf("\n");
-	return 0;
+printf("\n**Fibonacci Series**\n");
+printf("Name: Purbasa Seal, Class: MCA1A, Roll: 40\n");
+int a=0,b=1,s=0,n;
+printf("Enter the range : ");
+scanf("%d",&n);
+printf("Fibonacci Series =\n");
+printf("%d ",s);
+s=a+b;
+for(int i=2;i<=n;i++)
+{
+printf("%d ",s);
+s=a+b;
+
+a=b;
+b=s;
 }
-void update_array(int *arr,int size)
-{
-	for(int i=0;i<size;i++)
-	{
-		arr[i]+=15; //updating array by adding 15 with each element
-	}
-	printf("The array elements in the called function (After updation) :\n");
-	for(int i=0;i<size;i++)
-	{
-		printf("%d ",arr[i]);
-	}
-	printf("\n");
+printf("\n");
 }
