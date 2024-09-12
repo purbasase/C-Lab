9ff5ee7f5fff55113d3d498df1f89f5295368d9d
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-12 21:48:49 +0530

    Create 16.c

---
+++ Assignment 1/16.c
@@ -0,0 +1,15 @@
+//Write a C program to check whether a number is even or odd.
+
+#include<stdio.h>
+void main()
+{
+int n;
+printf("**Odd-Even**\n");
+printf("Name: Purbasa Seal, Class: MCA1A, Roll: 40\n");
+printf("Enter a number :");
+scanf("%d",&n);
+if(n%2==0)
+printf("Even Number\n");
+else
+printf("Odd Number\n");
+}
