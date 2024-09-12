d729a35c976a0069521a8d3813508034cb899198
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-12 21:29:40 +0530

    1.c

--- Assignment 1/1.c
+++ Assignment 1/1.c
@@ -1,28 +1,16 @@
+//Write a C program to find the sum and average of three numbers.
+
 #include<stdio.h>
-int main()
+void main()
 {
-int row,col,i,j,k=0;
-int arr[10][10],sm[100][3];
-printf("Enter number of rows: ");
-scanf("%d",&row);
-printf("Enter number of cols: ");
-scanf("%d",&col);
-printf("Enter no. of elements for the (%dX%d) matrics:\n",row,col);
-for(i=0;i<row;i++)
-{
-for(j=0;j<col;j++)
-{
-scanf("%d",&arr[i][j]);
-if(arr[i][j]!=0)
-{
-sm[k][0]=i;
-sm[k][1]=j;
-sm[k][2]=arr[i][j];
-k++;
-}
-}
-}
-printf("Row\tColumn\tValue\n");
-for(i=0;i<k;i++)
-printf("%d\t%d\t%d\n",sm[i][0],sm[i][1],sm[i][2]);
+printf("\n****Sum and Average of 3 Numbers****\n");
+printf("Name: Purbasa Seal, Class: MCA1A, Roll: 40\n");
+int a,b,c,sum=0;
+double avg=0;
+printf("Enter 3 numbers: ");
+scanf("%d %d %d",&a,&b,&c);
+sum=a+b+c; //Logic of adding the numbers
+avg=sum/3; //Logic for finding the average of numbers
+printf("\nSum = %d\n",sum);
+printf("Average = %f\n",avg);
 }
