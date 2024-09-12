49a9ed93fa90d0909c7788af4bb94df8b2aa05c5
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-12 21:41:51 +0530

    9.c

---
+++ Assignment 1/9.c
@@ -0,0 +1,10 @@
+//Write a program to print your name without using any semicolons in the program.
+
+#include<stdio.h>
+void main()
+
+{
+printf("\n**Print Name Without Any Semicolon**\n");
+printf("Name: Purbasa Seal, Class: MCA1A, Roll: 40\n");
+if(printf("My name is Purbasa Seal\n")){}
+}
