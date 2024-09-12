def7bd4f6945361e7cf0542818c35e10d128891b
Purbasa1_MCA_1st_A <87030156+purbasase@users.noreply.github.com>
2024-09-12 21:46:56 +0530

    Create 14.c

---
+++ Assignment 1/14.c
@@ -0,0 +1,17 @@
+//Write a C program to check whether a number is negative, positive, or zero.
+
+#include<stdio.h>
+void main()
+{
+int n;
+printf("**Positive Negetive or Zero**\n");
+printf("Name: Purbasa Seal, Class: MCA1A, Roll: 40\n");
+printf("Enter a number :");
+scanf("%d",&n);
+if(n<0)
+printf("Negetive Number\n");
+else if(n>0)
+printf("Positive Number\n");
+else
+printf("It is Zero\n");
+}
